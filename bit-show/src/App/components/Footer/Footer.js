d62d73f19c893a1e124e7e4bc1819163ac54810e
zoran94 <zorans94@hotmail.com>
2019-04-28 11:52:06 +0200

    bit show completed

--- bit-show/src/App/components/Footer/Footer.js
+++ bit-show/src/App/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
 
     return (
 
-    <footer className=" text-center text-white fixed-bottom">
+    <footer className=" text-center text-white">
         <div className="container center mt-2">
             <span>Copyright &copy; Bit Student 2019</span>
          </div>
